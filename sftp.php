069b6412ee13a4423a3d42e54d375dcfb7f973fd
rodrigosborges <gmrodrigoborges@gmail.com>
2018-01-05 11:07:57 -0200

    Projeto pronto

---
+++ sftp.php
@@ -0,0 +1,21 @@
+<?php
+
+	include 'sftp/Net/SFTP.php';
+
+    //IP do servidor
+	$strServer = "192.168.1.1";
+    //Usuário de acesso
+    $strServerUsername = "user";
+    //Senha de acesso
+    $strServerPassword = "123";
+    //Caminho do arquivo local
+    $file = 'folder/test.txt';
+    //Caminho do arquivo no servidor
+    $remote_file = 'html/test.txt';
+
+    $sftp = new NET_SFTP($strServer);
+
+    //Caso esteja tudo certo, irá salvar o arquivo quando entrar no IF.
+    if ($sftp->login($strServerUsername, $strServerPassword)) {
+        $sftp->put($remote_file, $file, NET_SFTP_LOCAL_FILE);
+    }
